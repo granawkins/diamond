550068c0c5acc06282192cb4d58bdb41131d73b6
Grant Hawkins <granthawkins88@gmail.com>
2025-10-07 15:31:26 +0700

    class and demo

--- diamond.py
+++ diamond.py
@@ -3,22 +3,29 @@ import board, busio
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
 
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = PCA9685(i2c)
-pca.frequency = 50
+class Diamond:
+    def __init__(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        pca = PCA9685(i2c)
+        pca.frequency = 50
 
-servo0 = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
-servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
+        servo0 = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
+        servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
 
-servo0.angle = 0
-servo1.angle = 0
+        servo0.angle = 0  # horizontal, left
+        servo1.angle = 0  # vertical, up
 
-time.sleep(1)
+        servo0.angle = 52
+        servo1.angle = 38  # legs perpendicular
 
-servo0.angle = 180
-servo1.angle = 180
-
-time.sleep(1)
-
-servo0.angle = 0
-servo1.angle = 0
+    def demo(self):
+        # Cycle continuously through all angles 45 to 135 in steps of 15
+        while True:
+            for angle in range(45, 135, 15):
+                self.servo0.angle = angle
+                self.servo1.angle = angle
+                time.sleep(0.1)
+            for angle in range(135, 45, -15):
+                self.servo0.angle = angle
+                self.servo1.angle = angle
+                time.sleep(0.1)
