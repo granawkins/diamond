b76a0d39439a72575fecef625ab2621ead3ab048
Grant Hawkins <granthawkins88@gmail.com>
2025-10-18 05:53:12 +0700

    multi channel

--- test_mg90.py
+++ test_mg90.py
@@ -4,23 +4,24 @@ import board, busio
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import servo
 
-def main(last_angle: int):
+def main(channel: int, last_angle: int):
 
     i2c = busio.I2C(board.SCL, board.SDA)
     pca = PCA9685(i2c)
     pca.frequency = 50
 
-    s = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
+    s = servo.Servo(pca.channels[channel], min_pulse=500, max_pulse=2500)
 
     s.angle = 1
-    time.sleep(2)
+    time.sleep(1)
     s.angle = 180
-    time.sleep(2)
+    time.sleep(1)
     s.angle = last_angle
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("last_angle", type=int, nargs="?", default=90)
+    parser.add_argument("channel", type=int, nargs="1", default=0)
+    parser.add_argument("last_angle", type=int, nargs="1", default=90)
     args = parser.parse_args()
-    main(args.last_angle)
+    main(args.channel[0], args.last_angle[0])
